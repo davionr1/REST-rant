f6346bb02460238469e1d0b4159313ca2a411965
Davion Roachford <davionr1@icloud.com>
2022-11-13 11:59:31 -0800

    new places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -3,28 +3,29 @@ const { Router } = require('express')
 const express = require('express')
 const router = express.Router()
 const app = express()
-
+const places = require('../models/places.js')
 router.get('/new', (req, res) => {
     res.render('places/new')
   })  
 // GET /places
-router.get('/', (req, res) => {
-  let places = [{
-    name: 'H-Thai-ML',
-    city: 'Seattle',
-    state: 'WA',
-    cuisines: 'Thai, Pan-Asian',
-    pic: 'http://placekitten.com/250/250'
-  }, {
-    name: 'Coding Cat Cafe',
-    city: 'Phoenix',
-    state: 'AZ',
-    cuisines: 'Coffee, Bakery',
-    pic: 'http://placekitten.com/250/250'
-  }]
-     res.render('places/index', { places })
-    
-  })
+router.post('/', (req, res) => {
+  console.log(req.body)
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.redirect('/places')
+})
+
+
+
   
   
   
